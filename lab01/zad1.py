66d76691e88b15b83079dad29fa1ae7029d7211b
Janek <j.brunow@lastlevel.pl>
2023-02-23 15:43:50 +0100

    Initial commit

---
+++ lab01/zad1.py
@@ -0,0 +1,8 @@
+var_int = 987654321123456789
+var_int2 = int(666666666666666666)
+var_float = 5.6
+var_float2 = float(6.5)
+print(type(var_int))
+print(type(var_int2))
+print(type(var_float))
+print(type(var_float2))
